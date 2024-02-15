06aa735c75022dae7a598c1f2c91de0da05be30e
jisooooooooooo <dlawltn0708@naver.com>
2024-02-15 18:08:22 +0900

    styles

--- reactchallengetypescript/src/index.tsx
+++ reactchallengetypescript/src/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { lighttheme } from "./theme";
+import App from "./App";
+import { theme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lighttheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
