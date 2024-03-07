9847bb2f68d0ae5131d49e5dff62ccc8b18c3428
jisooooooooooo <dlawltn0708@naver.com>
2024-03-07 17:15:16 +0900

    to do setup

--- reactchallengetypescript/src/theme.ts
+++ reactchallengetypescript/src/theme.ts
@@ -1,12 +1,7 @@
 import { DefaultTheme } from "styled-components";
-
 export const darkTheme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "black",
-  accentColor: "#4cd137",
-};
-export const lightTheme: DefaultTheme = {
-  bgColor: "whitesmoke",
-  textColor: "black",
-  accentColor: "#4cd137",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
 };
