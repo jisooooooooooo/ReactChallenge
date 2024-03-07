f82e2e89d489cf53e2242cdeb195b4bf7ec0805c
jisooooooooooo <dlawltn0708@naver.com>
2024-03-07 18:30:09 +0900

    custom validation

--- reactchallengetypescript/src/ToDoList.tsx
+++ reactchallengetypescript/src/ToDoList.tsx
@@ -28,6 +28,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -35,15 +36,23 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError("extraError", { message: "Server offline." });
   };
-
+  console.log(errors);
   return (
     <div>
       <form
@@ -62,7 +71,15 @@ function ToDoList() {
         />
         <span>{errors?.email?.message as string}</span>
         <input
-          {...register("firstName", { required: "write here" })}
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nick allowed" : true,
+            },
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message as string}</span>
@@ -98,6 +115,7 @@ function ToDoList() {
         <span>{errors?.password1?.message as string}</span>
 
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
